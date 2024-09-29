fa17729eb1a8dd598b722c6660018af77998818d
Fifla47 <164636161+Fifla47@users.noreply.github.com>
2024-09-29 13:54:29 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is was coded by Refilwe Phelwane and is{" "}
-        <a href="https://github.com/Fifla47/react-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is was coded by{" "}
+          <a href="https://github.com/Fifla47" target="_blank">
+            Refilwe Phelwane
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Fifla47/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
